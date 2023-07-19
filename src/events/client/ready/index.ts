fc84f8cd312af1c13e60d7b3b892ccbfe29e6a33
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2023-07-19 18:09:40 +0300

    .

--- src/events/client/ready/index.ts
+++ src/events/client/ready/index.ts
@@ -40,7 +40,7 @@ const Ready: Guard.IEvent = {
         );
 
         const document = (await GuildModel.findOne({ id: guild.id })) || (await GuildModel.create({ id: guild.id }));
-        client.servers.set(guild.id, { settings: { moderation: {}, guard: {}, ...document.toObject() } });
+        client.servers.set(guild.id, { settings: { moderation: {}, guard: {}, ...(document.settings || {}) } });
 
         client.utils.danger = document.settings.guard.danger;
 
