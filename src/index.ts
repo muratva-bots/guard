b23a8cfe29b2261e0c7fd98d43c2b18303720b69
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2023-08-05 23:02:22 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ client.connect();
 const selfClient = new SelfClient();
 selfClient.start().then((value) => (client.utils.vanityClient = value));
 
-/*
 process.on('unhandledRejection', (error: Error) => console.log(`[ERROR]: ${error.name}: ${error.message}`));
 process.on('uncaughtException', (error: Error) => console.log(`[ERROR]: ${error.name}: ${error.message}`));
-*/
+
