84632dae388893599fb3c9142185a4b29107581b
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2023-08-26 23:02:03 +0300

    .

--- src/models/guildModel.ts
+++ src/models/guildModel.ts
@@ -79,8 +79,56 @@ export class GuildClass {
     @prop({ type: Object, default: {} })
     public guard: GuardClass;
 
-    @prop({ type: Object, default: {} })
+    @prop({
+        type: Object,
+        default: {
+            messagePoint: 1,
+            messageStaffPoint: 2,
+            invitePoint: 70,
+            sleepPoint: 4,
+            publicPoint: 8,
+            meetingPoint: 500,
+            noMute: true,
+            eventFinishTimestamp: Date.now(),
+            staffTakePoints: 70,
+            taggedPoints: 70
+        },
+    })
     public point: object;
+
+    @prop({
+        type: Object,
+        default: {
+            removeOldRank: false,
+            dailyPublic: 0,
+            lastPublic: 0,
+            dailyStream: 0,
+            lastStream: 0,
+            dailyCam: 0,
+            lastCam: 0,
+            dailyStreamOpen: 0,
+            lastStreamOpen: 0,
+            dailyCamOpen: 0,
+            lastCamOpen: 0,
+            dailyGeneral: 0,
+            lastGeneral: 0,
+            dailyMessage: 0,
+            lastMessage: 0,
+            dailyAfk: 0,
+            lastAfk: 0,
+            dailyJoin: 0,
+            lastJoin: 0,
+            dailyLeave: 0,
+            lastLeave: 0,
+            camChannels: [],
+            dailyVoice: 0,
+            lastVoice: 0,
+            lastDay: new Date().setHours(0, 0, 0, 0),
+            days: 1,
+            owneredStreams: []
+        },
+    })
+    public stat: object;
 }
 
 export const GuildModel = getModelForClass(GuildClass);
